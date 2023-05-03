bc28143a6470cdd445cafcc92303cb528956bfda
kksbz <hellghost7@naver.com>
2023-05-04 01:16:20 +0900

    [KKS] 상점 시스템 구매, 판매 setup

--- ProjectSL/Assets/KKS/Scripts/UiManager.cs
+++ ProjectSL/Assets/KKS/Scripts/UiManager.cs
@@ -12,6 +12,7 @@ public class UiManager : Singleton<UiManager>
     public WarpController warp; // 화톳불 워프 컨트롤러
     public LoadingPanel loadingPanel; // 로딩 패널
     public GameObject levelUpPanel; // 레벨업 패널
+    public GameObject shopPanel; // 상점 패널
     public StatusPanel statusPanel; // 스테이터스 패널
     public StatusPanel invenStatusPanel; // 인벤토리 스테이터스 패널
     public OptionPanel optionPanel; // 옵션 패널
